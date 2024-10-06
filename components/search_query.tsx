041a59763e5b028547fe8c7aa763d9fd10f533e4
Oli Young <oliyoung@gmail.com>
2024-10-06 12:10:58 +1030

    SearchQuery formatting

--- components/search_query.tsx
+++ components/search_query.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react"
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+
 
 interface SearchQueryParams {
     onSearch: (searchContext: SearchContext) => void
 }
 
-const TRIP_TO_THE_MOON_RELEASE = 1902
+const YEAR_START = 1902
+const YEAR_END = new Date().getFullYear()
 
 const SearchQuery = ({ onSearch }: SearchQueryParams) => {
-
     const queryTypes: SearchQueryTypeStrings[] = [
         "any", "movie", "series", "episode"
     ]
@@ -29,28 +31,37 @@ const SearchQuery = ({ onSearch }: SearchQueryParams) => {
         onSearch(searchContext)
     }, [searchContext]);
 
-    return <div id="searchQuery" className="bg-zinc-500 col-span-3 col-start-1 grid grid-cols-4">
-        <input className="text-white bg-zinc-500 p-2 col-span-2" type="search" id="searchQuery_Title" name='title' onChange={onChange} />
+    return <div id="searchQuery" className="bg-zinc-500 col-span-3 col-start-1 grid grid-cols-4 content-center">
+        <div id="searchQuery_Title" className=" col-span-2 flex flex-row px-4 items-center min-w-full ">
+            <MagnifyingGlassIcon width={30} height={30} className="text-white fill-white stroke-white" />
+            <input name='title' onChange={onChange} className="w-full text-white bg-zinc-500 p-2 focus:outline-none focus:ring-none focus:border-none" type="search" />
+        </div>
         <div id="searchQuery_Year" className="text-white">
             <h6 className="text-xs uppercase">{'year'}</h6>
-            {searchContext.year}
-            <input
-                max={new Date().getFullYear()}
-                min={TRIP_TO_THE_MOON_RELEASE}
-                name="year"
-                onChange={onChange}
-                type="range"
-                value={searchContext.year}
-            />
+            <div className="flex flex-row gap-4">
+                <input
+                    max={YEAR_END}
+                    min={YEAR_START}
+                    name="year"
+                    onChange={onChange}
+                    type="range"
+                    value={searchContext.year}
+                />
+                <span className="text-xs">{searchContext.year}</span>
+            </div>
         </div>
         <div id="searchQuery_Type" className="text-white">
             <h6 className="text-xs uppercase">{'type'}</h6>
-            <div className="flex flex-row gap-3">
+            <div className="flex flex-row gap-4">
                 {queryTypes.map(queryType => {
-                    return <label className="text-xs capitalize" key={queryType}>
-                        <input className="bg-zinc-500" onChange={onChange} type='radio' name="type" value={queryType} />
-                        {queryType}
-                    </label>
+                    return <div className="inline-flex items-center gap-1">
+                        <label className="relative flex items-center cursor-pointer" key={queryType}>
+                            <input id={`${queryType}_type`} name="type" onChange={onChange} type='radio' value={queryType} className="peer h-3 w-3 cursor-pointer appearance-none rounded-full border border-zinc-300 checked:border-zinc-300 transition-all" />
+                            <span className="absolute bg-zinc-300 w-2 h-2 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                            </span>
+                        </label>
+                        <span className="inline-block capitalize text-xs">{queryType}</span>
+                    </div>
                 })}
             </div>
         </div>
